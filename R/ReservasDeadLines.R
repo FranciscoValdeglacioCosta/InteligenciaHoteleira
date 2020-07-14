07526fe75cfc69a9b8fb9df0fbe0c3b1ba35ed82
FranciscoValdeglacioCosta <67345800+FranciscoValdeglacioCosta@users.noreply.github.com>
2020-07-14 18:27:08 -0300

    Commit Auxiliar

--- R/ReservasDeadLines.R
+++ R/ReservasDeadLines.R
@@ -1,19 +1,81 @@
-install.packages("odbc")
+install.packages("RODBC")
 library(odbc)
 
+# Conectando no banco de dados SQLSERVER
+
+con <- odbcDriverConnect('driver={SQL Server};server=localhost;database=DM_Reservas;trusted_connection=true')
 
 
-library(odbc)
-con <- dbConnect(odbc(),
-                 Driver = "SQL Server",
-                 Server = "localhost",
-                 Database = "CMT",
-                 UID = "cm",
-                 PWD = "cm",
-                 Port = 1433)
-                 
+# Extraindo as tabelas
+DimHoteis                 = sqlQuery(con, "SELECT IDHOTEL, HOTEL FROM dbo.DimHoteis;")
+DimOrigem                 = sqlQuery(con, "SELECT IDORIGEM,DESCRICAO FROM dbo.DimOrigem;")
+DimStatusReservas         = sqlQuery(con, "SELECT IDSTATUSRESERVA,STATUSRESERVAS FROM dbo.DimStatusReservas;")
+DimTipoUH                 = sqlQuery(con, "SELECT IDTIPOUH,DESCRICAO FROM dbo.DimTipoUH;")
+ftHospede                 = sqlQuery(con, "SELECT IDHOSPEDE, NOMEHOSPEDE, SEXO, UF FROM dbo.ftHospede;")
+ftReservas                = sqlQuery(con, "SELECT * FROM dbo.ftReservas;")
 
-alltables = dbListTables(con)
-alltables
-                 
-                 
+View(DimHoteis)
+View(DimOrigem)
+View(DimTipoUH)
+View(ftHospede)
+View(ftReservas) 
+
+
+
+
+#unidos as tabelas
+library(dplyr)
+
+
+df <- DimHoteis %>%
+inner_join(ftReservas, by = c("IDHOTEL")) %>%
+inner_join(DimOrigem,  by = c("IDORIGEM")) %>%
+inner_join(DimStatusReservas,  by = c("IDSTATUSRESERVA")) %>%
+inner_join(DimTipoUH,  by = c("IDTIPOUH")) %>%
+inner_join(ftHospede,  by = c("IDHOSPEDE")) %>%
+#renomenando colunas
+rename(TIPOUH=DESCRICAO.y) %>%
+rename(ORIGEM=DESCRICAO.x) 
+
+
+#ajustando dados
+df$DTRESERVA      <-  as.Date(df$DTRESERVA)
+df$DTDATACHEGADA  <-  as.Date(df$DTDATACHEGADA)
+df$DTPARTIDA      <-  as.Date(df$DTPARTIDA)
+df$DTRESERVA      <-  as.Date(df$DTRESERVA)
+
+#Remover desnecessario
+df$IDHOTEL <- NULL
+df$IDORIGEM <- NULL
+df$IDRESERVA <- NULL
+df$IDSTATUSRESERVA <- NULL
+df$IDTIPOUH <- NULL
+df$IDHOSPEDE <- NULL
+df$IDTIPOUH <- NULL
+
+#Check 
+str(df)
+
+View(df)
+
+#valores Medio Diaria
+
+hist(df$VLRDIARIA,breaks = 4)
+
+plot(df$UF)
+
+
+
+
+
+
+#gravar no sqllite a analise feita 
+
+install.packages
+
+install.packages("RSQLite")
+
+
+
+
+
